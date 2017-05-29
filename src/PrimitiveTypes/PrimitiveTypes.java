18024680089fae9fb833d12c2b861a167ad9f620
louisscruz <louisstephancruz@me.com>
2017-05-28 20:15:39 -0700

    Add other parity methods and right propagate

--- src/PrimitiveTypes/PrimitiveTypes.java
+++ src/PrimitiveTypes/PrimitiveTypes.java
@@ -13,6 +13,7 @@ public class PrimitiveTypes {
         return numBits;
     }
 
+    // O(n) time, where n is the length of the word
     public static short parity(long x) {
         short result = 0;
         while(x != 0) {
@@ -22,9 +23,38 @@ public class PrimitiveTypes {
         return result;
     }
 
+    // O(s) time, where s is the number of set bits in the word
+    public static short fasterParity(long x) {
+        short result = 0;
+        while (x != 0) {
+            result ^= 1;
+            x &= (x - 1);
+        }
+        return result;
+    }
+
+    // O(log(n)) time, where n is the length of the word
+    public static short evenFasterParity(long x) {
+        x ^= x >>> 32;
+        x ^= x >>> 16;
+        x ^= x >>> 8;
+        x ^= x >>> 4;
+        x ^= x >>> 2;
+        x ^= x >>> 1;
+        return (short)(x & 0x1);
+    }
+
+    // O(1) time
+    public static long rightPropagate(long x) {
+        return x | ((x & ~(x - 1)) - 1);
+    }
+
     public static void main(String[] args) {
-        PrimitiveTypes klass = null;
-        System.out.println(klass.countBits(5));
-        System.out.println(klass.parity(123121));
+        PrimitiveTypes primitiveTypes = null;
+        System.out.println(primitiveTypes.countBits(5) == 2);
+        System.out.println(primitiveTypes.parity(10000) == 1);
+        System.out.println(primitiveTypes.fasterParity(10000) == 1);
+        System.out.println(primitiveTypes.evenFasterParity(10000) == 1);
+        System.out.println(primitiveTypes.rightPropagate(80) == 95);
     }
 }

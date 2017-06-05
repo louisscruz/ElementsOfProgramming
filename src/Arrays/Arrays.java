7606de8166fb336c9f0a5146076f30806a43e9d9
louisscruz <louisstephancruz@me.com>
2017-06-05 12:02:27 -0700

    Add bitStringAdder

--- src/Arrays/Arrays.java
+++ src/Arrays/Arrays.java
@@ -82,8 +82,68 @@ public class Arrays {
         }
     }
 
-    public static void sameKeyDutchFlagPartition(List<Color> A) {
+    // O(n) time
+    public static List<Integer> plusOne(List<Integer> A) {
+        int n = A.size() - 1;
 
+        A.set(n, A.get(n) + 1);
+
+        for (int i = n; i > 0 && A.get(i) == 10; --i) {
+            A.set(i, 0);
+            A.set(i - 1, A.get(i - 1) + 1);
+        }
+
+        if (A.get(0) == 10) {
+            A.set(0, 0);
+            A.add(0, 1);
+        }
+
+        return A;
+    }
+
+    public static String bitStringAdd(String a, String b) {
+        if (a.length() != b.length()) throw new IllegalArgumentException();
+
+        List<Integer> result = new ArrayList<Integer>();
+
+        for (int i = 0; i < a.length(); i++) {
+            char first = a.charAt(i);
+            char second = b.charAt(i);
+
+            if (!(first == '1' || first == '0') || !(second == '1' || second == '0')) throw new IllegalArgumentException();
+
+            int firstValue = first == '1' ? 1 : 0;
+            int secondValue = second == '1' ? 1 : 0;
+            int newValue = firstValue + secondValue;
+
+            result.add(newValue);
+        }
+
+        for (int i = result.size() - 1; i > 0; i--) {
+            int carryOver = result.get(i) / 2;
+            int remainder = result.get(i) % 2;
+
+            if (carryOver > 0) {
+                result.set(i, remainder);
+                result.set(i - 1, result.get(i - 1) + carryOver);
+            }
+        }
+
+        if (result.get(0) > 1) {
+            int carryOver = result.get(0) / 2;
+            int remainder = result.get(0) % 2;
+
+            result.set(0, remainder);
+            result.add(0, carryOver);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i : result) {
+            sb.append(i);
+        }
+
+        return sb.toString();
     }
 
     public static void main(String[] args) {
@@ -112,5 +172,13 @@ public class Arrays {
         System.out.println(colors1.equals(colorsAnswer));
         System.out.println(colors2.equals(colorsAnswer));
         System.out.println(colors3.equals(colorsAnswer));
+
+        List<Integer> decimalList = new ArrayList<Integer>();
+        decimalList.add(1);
+        decimalList.add(2);
+        decimalList.add(9);
+        System.out.println(plusOne(decimalList).get(1) == 3);
+
+        System.out.println(bitStringAdd("1111", "1111").equals("11110"));
     }
 }

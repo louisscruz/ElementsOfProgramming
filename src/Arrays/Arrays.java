538a7e94ec9fe7b8ef5a1c87dba68e44ac7b891f
louisscruz <louisstephancruz@me.com>
2017-06-11 19:27:52 -0700

    Add primes

--- src/Arrays/Arrays.java
+++ src/Arrays/Arrays.java
@@ -395,7 +395,27 @@ public class Arrays {
         }
     }
 
-//    public
+    // O(n log log n) time, O(n) space
+    public static List<Integer> primes(int n) {
+        final int size = (int)Math.floor(0.5 * (n - 3)) + 1;
+
+        List<Integer> primes = new ArrayList<>();
+        primes.add(2);
+
+        List<Boolean> isPrime = new ArrayList<>(Collections.nCopies(size, true));
+        for (int i = 0; i < size; i++) {
+            if (isPrime.get(i)) {
+                int p = ((i * 2) + 3);
+                primes.add(p);
+
+                for (long j = ((i * i) * 2) + 6 * i + 3; j < size; j += p) {
+                    isPrime.set((int)j, false);
+                }
+            }
+        }
+
+        return primes;
+    }
 
     public static void main(String[] args) {
         int[] array1 = new int[]{1, 2, 4, 3};
@@ -529,5 +549,9 @@ public class Arrays {
         }
 
         System.out.println(properlyArranged);
+
+        List<Integer> primeList = new ArrayList<>(java.util.Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19));
+
+        System.out.println(primeList.equals(primeList));
     }
 }

c6ee9795c14cc1a483cb17e87095a03f79e1c14d
louisscruz <louisstephancruz@me.com>
2017-06-19 23:33:53 -0700

    Add max stack

--- src/StacksAndQueues/StacksAndQueues.java
+++ src/StacksAndQueues/StacksAndQueues.java
@@ -1,4 +1,44 @@
 package StacksAndQueues;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class StacksAndQueues {
+    private static class ElementWithCachedMax {
+        public Integer element;
+        public Integer max;
+
+        public ElementWithCachedMax(Integer element, Integer max) {
+            this.element = element;
+            this.max = max;
+        }
+    }
+
+    public static class Stack {
+        private Deque<ElementWithCachedMax> elementWithCachedMax = new LinkedList<>();
+
+        public boolean empty() {
+            return elementWithCachedMax.isEmpty();
+        }
+
+        public Integer max() {
+            if (empty()) {
+                throw new IllegalStateException("max(): empty stack");
+            }
+
+            return elementWithCachedMax.peek().max;
+        }
+
+        public Integer pop() {
+            if (empty()) {
+                throw new IllegalStateException("pop(): empty stack");
+            }
+
+            return elementWithCachedMax.removeFirst().element;
+        }
+
+        public void push(Integer x) {
+            elementWithCachedMax.addFirst(new ElementWithCachedMax(x, Math.max(x, empty() ? x : max())));
+        }
+    }
 }
--- src/StacksAndQueues/StacksAndQueuesTest.java
+++ src/StacksAndQueues/StacksAndQueuesTest.java
@@ -1,7 +1,29 @@
 package StacksAndQueues;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StacksAndQueuesTest {
+    @Nested
+    class maxStackTests {
+        @Test
+        @DisplayName("properly returns the max")
+        void maxStack() {
+            StacksAndQueues.Stack stack = new StacksAndQueues.Stack();
+            stack.push(1);
+            stack.push(3);
+            stack.push(2);
+
+            assertEquals(3, (int)stack.max());
+
+            stack.pop();
+            stack.pop();
+
+            assertEquals(1, (int)stack.pop());
+        }
+    }
 
 }
